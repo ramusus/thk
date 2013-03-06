22c8e7cd6a00a591a192dd5b0550d9336f75bbbb
ramusus <ramusus@gmail.com>
2013-03-06 13:57:48 +0530

    deleted db config, updated deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,24 +3,31 @@ require 'rvm/capistrano'
 require 'bundler/capistrano'
 #require 'capistrano/deepmodules'
 
-set :application, "thk"
-set :deploy_to, "/var/home/hosting_abbb/projects/thk"
-set :deploy_via, :remote_cache
-set :use_sudo, true
-set :user, "hosting_abbb"
-set :unicorn_conf, "#{deploy_to}/current/config/unicorn.rb"
-set :unicorn_pid, "#{deploy_to}/shared/pids/unicorn.pid"
+ssh_options[:forward_agent] = true
+
+set :application,     "tver-hockey-club"
+set :deploy_server,   "neon.locum.ru"
+
+set :bundle_without,  [:development, :test]
+
+set :user,            "hosting_abbb"
+set :login,           "abbb"
+set :use_sudo,        false
+set :deploy_to,       "/home/#{user}/projects/#{application}"
+set :unicorn_conf,    "/etc/unicorn/#{application}.#{login}.rb"
+set :unicorn_pid,     "/var/run/unicorn/#{application}.#{login}.pid"
+set :bundle_dir,      File.join(fetch(:shared_path), 'gems')
+role :web,            deploy_server
+role :app,            deploy_server
+role :db,             deploy_server, :primary => true
 
 set :rvm_ruby_string, "1.9.2"
-set :rvm_type, :user
+set :rake,            "rvm use #{rvm_ruby_string} do bundle exec rake"
+set :bundle_cmd,      "rvm use #{rvm_ruby_string} do bundle"
 
 set :scm, "git"
 set :repository,  "git@github.com:ramusus/thk.git"
 set :branch, "master"
-
-role :web, "neon.locum.ru"                          # Your HTTP server, Apache/etc
-role :app, "neon.locum.ru"                          # This may be the same as your `Web` server
-role :db,  "neon.locum.ru", :primary => true # This is where Rails migrations will run
 
 set :css_files, [
   'all.css',
@@ -40,15 +47,37 @@ default_run_options[:pty] = true
 
 after "bundle:install", "deploy:auto_migrate"
 
+## Чтобы не хранить database.yml в системе контроля версий, поместите
+## dayabase.yml в shared-каталог проекта на сервере и раскомментируйте
+## следующие строки.
+after "deploy:update_code", :copy_database_config
+task :copy_database_config, roles => :app do
+ db_config = "#{shared_path}/database.yml"
+ run "cp #{db_config} #{release_path}/config/database.yml"
+end
+
+before 'deploy:finalize_update', 'set_current_release'
+task :set_current_release, :roles => :app do
+    set :current_release, latest_release
+end
+
+set :unicorn_start_cmd, "(cd #{deploy_to}/current; rvm use #{rvm_ruby_string} do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+
+# - for unicorn - #
 namespace :deploy do
-  task :start do
-    run "bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D"
+  desc "Start application"
+  task :start, :roles => :app do
+    run unicorn_start_cmd
   end
-  task :stop do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -QUIT `cat #{unicorn_pid}`; fi"
-   end
-  task :restart, :roles => :app, :except => { :no_release => true } do
-    run "if [ -f #{unicorn_pid} ] && [ -e /proc/$(cat #{unicorn_pid}) ]; then kill -USR2 `cat #{unicorn_pid}`; else cd #{deploy_to}/current && bundle exec unicorn -c #{unicorn_conf} -E #{rails_env} -D; fi"
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
   task :auto_migrate do
     rake = fetch(:rake, "rake")
