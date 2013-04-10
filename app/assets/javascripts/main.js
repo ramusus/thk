966e1b6f75df3d9f082b683d6191619227a97918
ramusus <ramusus@gmail.com>
2013-04-10 22:47:01 +0300

    merged with new js

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,79 +1,84 @@
+var init_sliders = function() {
+
+    $('.slider').each(function() {
+        var $box = $(this),
+            $sliderNav = $box.find('.slider-nav'),
+            $sliderBox = $box.find('.slider-list'),
+            $sliderItems = $sliderBox.find('.slider-item'),
+            $sliderNavPrev = $sliderNav.find('.slider-nav-prev'),
+            $sliderNavNext = $sliderNav.find('.slider-nav-next'),
+            $sliderNavCur = $sliderNav.find('.slider-nav-num'),
+            $sliderNavTotal = $sliderNav.find('.slider-nav-of'),
+
+            CLASS_CURRENT = 'current',
+            CLASS_INVERTED = 'inverted';
+
+        $sliderNavTotal.text($sliderItems.length);
+
+        var show = function($item) {
+            $sliderItems.fadeOut();
+            $item.fadeIn();
+            $sliderNavCur.text($item.index() + 1);
+            $sliderNav.removeClass(CLASS_INVERTED);
+            if ($item.hasClass(CLASS_INVERTED)) {
+                $sliderNav.addClass(CLASS_INVERTED);
+            }
+        }
+
+        $sliderNavNext.on('click', function() {
+            $cur = $cur.next();
+            if (!$cur.length) $cur = $sliderItems.first();
+            show($cur);
+        });
+
+        $sliderNavPrev.on('click', function() {
+            $cur = $cur.prev();
+            if (!$cur.length) $cur = $sliderItems.last();
+            show($cur);
+        });
+
+        $sliderBox.on('click', function() {
+            $sliderNavNext.trigger('click');
+        });
+
+        var $cur = $sliderBox.find('.' + CLASS_CURRENT);
+        show($cur);
+    });
+}
+
 $(function() {
-	$('.tabs').each(function() {
-		var $box = $(this),
-			$clickers = $box.find('.tabs-switcher-item'),
-			$tabs = $box.find('.tabs-tab'),
+    $('.tabs').each(function() {
+        var $box = $(this),
+            $clickers = $box.find('.tabs-switcher-item'),
+            $tabs = $box.find('.tabs-tab'),
 
-			CLASS_ACTIVE = 'active';
+            CLASS_ACTIVE = 'active';
 
-		$clickers.on('click', function() {
-			var index = $(this).index();
-			$box.find('.' + CLASS_ACTIVE).removeClass(CLASS_ACTIVE);
-			$(this).addClass(CLASS_ACTIVE);
-			$tabs.eq(index).addClass(CLASS_ACTIVE);
-		})
-	})
+        $clickers.on('click', function() {
+            var index = $(this).index();
+            $box.find('.' + CLASS_ACTIVE).removeClass(CLASS_ACTIVE);
+            $(this).addClass(CLASS_ACTIVE);
+            $tabs.eq(index).addClass(CLASS_ACTIVE);
+        })
+    })
 
-	$('.slider').each(function() {
-		var $box = $(this),
-			$sliderNav = $box.find('.slider-nav'),
-			$sliderBox = $box.find('.slider-list'),
-			$sliderItems = $sliderBox.find('.slider-item'),
-			$sliderNavPrev = $sliderNav.find('.slider-nav-prev'),
-			$sliderNavNext = $sliderNav.find('.slider-nav-next'),
-			$sliderNavCur = $sliderNav.find('.slider-nav-num'),
-			$sliderNavTotal = $sliderNav.find('.slider-nav-of'),
+    init_sliders();
 
-			CLASS_CURRENT = 'current',
-			CLASS_INVERTED = 'inverted';
-
-		$sliderNavTotal.text($sliderItems.length);
-
-		var show = function($item) {
-			$sliderItems.fadeOut();
-			$item.fadeIn();
-			$sliderNavCur.text($item.index() + 1);
-			$sliderNav.removeClass(CLASS_INVERTED);
-			if ($item.hasClass(CLASS_INVERTED)) {
-				$sliderNav.addClass(CLASS_INVERTED);
-			}
-		}
-
-		$sliderNavNext.on('click', function() {
-			$cur = $cur.next();
-			if (!$cur.length) $cur = $sliderItems.first();
-			show($cur);
-		});
-
-		$sliderNavPrev.on('click', function() {
-			$cur = $cur.prev();
-			if (!$cur.length) $cur = $sliderItems.last();
-			show($cur);
-		});
-
-		$sliderBox.on('click', function() {
-			$sliderNavNext.trigger('click');
-		});
-
-		var $cur = $sliderBox.find('.' + CLASS_CURRENT);
-		show($cur);
-	});
-
-	var $stick = $('.stick');
-	$(window).scroll(function(){
-		var y = $(window).scrollTop();
-		if (y > 200 && y < 400) {
-			$stick.hide();
-		}
-		else {
-			if (y > 400) {
-				$stick.fadeIn();
-				$stick.addClass('sticky');
-			}
-			else {
-				$stick.show();
-				$stick.removeClass('sticky');
-			}
-		}
-	});
+    var $stick = $('.stick');
+    $(window).scroll(function(){
+        var y = $(window).scrollTop();
+        if (y > 200 && y < 400) {
+            $stick.hide();
+        }
+        else {
+            if (y > 400) {
+                $stick.fadeIn();
+                $stick.addClass('sticky');
+            }
+            else {
+                $stick.show();
+                $stick.removeClass('sticky');
+            }
+        }
+    });
 });
